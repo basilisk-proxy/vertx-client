b5e2eda85b0b6fe183ae526d7f275f4a3632370e
the-infinite <toby.johnson.precious@gmail.com>
2026-05-14 19:46:24 +0100

    fix: request flight issues.

--- src/main/java/com/milestone/basilisk/vertx/GatewayApiClient.java
+++ src/main/java/com/milestone/basilisk/vertx/GatewayApiClient.java
@@ -1,11 +1,14 @@
 package com.milestone.basilisk.vertx;
 
+import java.util.Objects;
+
 import io.vertx.core.Future;
+import io.vertx.core.MultiMap;
 import io.vertx.core.Vertx;
-import io.vertx.core.buffer.Buffer;
 import io.vertx.core.http.HttpClientAgent;
 import io.vertx.core.http.HttpClientRequest;
 import io.vertx.core.http.HttpMethod;
+import io.vertx.core.http.RequestOptions;
 import io.vertx.core.json.JsonObject;
 
 @SuppressWarnings("unused")
@@ -19,19 +22,19 @@ public class GatewayApiClient {
     }
 
     public Future<JsonObject> registerInstanceAuto(JsonObject request) {
-        var copy = request.copy();
+        var copy = Objects.requireNonNullElse(request.copy(), new JsonObject());
         copy.getJsonObject("instance").put("instanceId", "");
         return registerInstance(copy);
     }
 
     public Future<JsonObject> registerInstance(JsonObject request) {
-        return webClient.request(HttpMethod.POST, baseUrl + "/registry/register")
-            .compose(httpClientRequest -> httpClientRequest.send(request.toBuffer()))
-            .compose(resp -> resp.body().map(Buffer::toJsonObject));
+        return webClient.request(new RequestOptions().setMethod(HttpMethod.POST).setAbsoluteURI(baseUrl + "/registry/register").setHeaders(MultiMap.caseInsensitiveMultiMap().add("Content-Type", "application/json")))
+            .compose(httpClientRequest -> httpClientRequest.send(request.encode()))
+            .compose(resp -> resp.body().map(buffer -> buffer.toJsonObject()));
     }
 
     public Future<Void> deregisterInstance(String serviceId, String instanceId) {
-        return webClient.request(HttpMethod.DELETE, baseUrl + "/registry/services/" + serviceId +"/instances/" + instanceId)
+        return webClient.request(new RequestOptions().setMethod(HttpMethod.DELETE).setAbsoluteURI(baseUrl + "/registry/services/" + serviceId + "/instances/" + instanceId).setHeaders(MultiMap.caseInsensitiveMultiMap().add("Content-Type", "application/json")))
                 .compose(HttpClientRequest::send)
                 .compose(resp -> resp.body().mapEmpty());
     }
